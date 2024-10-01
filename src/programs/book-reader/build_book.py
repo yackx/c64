267649bc4f6081002a9ff47314735159ccb22d5c
Youri Ackx <1193689+yackx@users.noreply.github.com>
2024-10-01 12:26:01 +0200

    Parameterize width
    Allow c128 80col

--- src/programs/book-reader/build_book.py
+++ src/programs/book-reader/build_book.py
@@ -8,6 +8,7 @@ import textwrap
 
 from scripts.ascii_converter import ascii_to_petscii_lower
 
+WIDTH = 40  # 40 for c64, preferably 80 for c128
 BOOK_DIR = 'thoreau'
 BOOK_FILE = 'disobedience.txt'
 BOOK_INFO = 'info.bin'
@@ -17,7 +18,7 @@ def build():
     with open(os.path.join(BOOK_DIR, BOOK_FILE), 'r') as f_in:
         lines = f_in.readlines()
 
-    wrapped_texts = [textwrap.wrap(line, width=40) for line in lines]
+    wrapped_texts = [textwrap.wrap(line, width=WIDTH) for line in lines]
     wrapped_texts_with_blanks = [wl if wl != [] else [''] for wl in wrapped_texts]
     wrapped_lines = [item for sublist in wrapped_texts_with_blanks for item in sublist]
     current_line_number = 0
@@ -25,7 +26,7 @@ def build():
 
     while current_line_number < len(wrapped_lines):
         page = wrapped_lines[current_line_number:current_line_number+24]
-        single_str = ''.join(line + ('\r' if len(line) < 40 else '') for line in page)
+        single_str = ''.join(line + ('\r' if len(line) < WIDTH else '') for line in page)
         petscii_codes: [int] = ascii_to_petscii_lower(single_str)
         hex = f'{current_page_number:02x}'
         with open(os.path.join(BOOK_DIR, f'page{hex}.pet'), 'wb') as f_out:
